0ef9065d2fdee6ea4ec5a41cb212b4f0d7f31a8c
babywade <yixiaofei@stu.xjtu.edu.cn>
2018-03-29 21:22:36 +0800

    update

--- cppConsole/main.cpp
+++ cppConsole/main.cpp
@@ -1,28 +1,40 @@
-//一维近似装箱问题
+//二维近似装箱问题
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <utility>
 
 using namespace std;
 //箱子体积
-#define VOLUMN 10
+#define VOLUMN_X 10
+#define VOLUMN_Y 10
+
 //联机算法
-void nextFit(int block, vector<int>& answer, int& cnt, vector<int>& volumns);
-void firstFit(int block, vector<int>& answer, int& cnt, vector<int>& volumns);
-void bestFit(int block, vector<int>& answer, int& cnt, vector<int>& volumns);
+void nextFit(pair<int, int>& block, vector<int>& answer, int& cnt, vector<pair<int, int>>& volumns);
+void firstFit(int& block, vector<int>& answer, int& cnt, vector<int>& volumns);
+void bestFit(int& block, vector<int>& answer, int& cnt, vector<int>& volumns);
+//脱机算法
+void firstFitDecreasing(vector<int>& blocks, vector<int>& answer, int& cnt, vector<int>& volumns);
+void bestFitDecreasing(vector<int>& blocks, vector<int>& answer, int& cnt, vector<int>& volumns);
 
 int main() {
-    int block;
+    pair<int, int> block;
     vector<int> answer;
-    vector<int> volumns;
+    vector<pair<int, int>> volumns;
     //#1箱子初始化为VOLUMN
-    volumns.push_back(VOLUMN);
+    volumns.push_back({VOLUMN_X, VOLUMN_Y});
     int cnt = 1;
+    vector<pair<int, int>> blocks;
 
     while(cin >> block) {
-//        nextFit(block, answer, cnt, volumns);
+        nextFit(block, answer, cnt, volumns);
 //        firstFit(block, answer, cnt, volumns);
-        bestFit(block, answer, cnt, volumns);
+//        bestFit(block, answer, cnt, volumns);
+
+//        blocks.push_back(block);
     }
+//    firstFitDecreasing(blocks, answer, cnt, volumns);
+
     //输出结果
     for(auto& s:answer) {
         cout << s << " ";
@@ -34,18 +46,21 @@ int main() {
     return 0;
 }
     //下项适应算法
-void nextFit(int block, vector<int>& answer, int& cnt, vector<int>& volumns) {
+void nextFit(pair<int, int>& block, vector<int>& answer, int& cnt, vector<pair<int, int>>& volumns) {
+    //需要定义>=操作符
     if(volumns[cnt - 1] >= block) {
         answer.push_back(cnt);
+        //需要定义-=操作符
         volumns[cnt - 1] -= block;
     } else {
         ++cnt;
         answer.push_back(cnt);
-        volumns.push_back(VOLUMN - block);
+        //需要定义-操作符
+        volumns.push_back({VOLUMN_X, VOLUMN_Y} - block);
     }
 }
     //首次适应算法
-void firstFit(int block, vector<int>& answer, int& cnt, vector<int>& volumns) {
+void firstFit(int& block, vector<int>& answer, int& cnt, vector<int>& volumns) {
     for(vector<int>::iterator iter = volumns.begin(); iter != volumns.end(); ++iter) {
         if((*iter) >= block) {
             answer.push_back(iter - volumns.cbegin() + 1);
@@ -59,7 +74,7 @@ void firstFit(int block, vector<int>& answer, int& cnt, vector<int>& volumns) {
     answer.push_back(cnt);
 }
     //最佳适应算法
-void bestFit(int block, vector<int>& answer, int& cnt, vector<int>& volumns) {
+void bestFit(int& block, vector<int>& answer, int& cnt, vector<int>& volumns) {
     int bestPosition = cnt + 1;
     int min_volumn = volumns[0];
     for(vector<int>::iterator iter = volumns.begin(); iter != volumns.end(); ++iter) {
@@ -77,5 +92,19 @@ void bestFit(int block, vector<int>& answer, int& cnt, vector<int>& volumns) {
     } else {
         answer.push_back(bestPosition);
         volumns[bestPosition - 1] -= block;
+    }
+}
+
+void firstFitDecreasing(vector<int>& blocks, vector<int>& answer, int& cnt, vector<int>& volumns) {
+    sort(blocks.begin(), blocks.end());
+    for(auto& block:blocks) {
+        firstFit(block, answer, cnt, volumns);
+    }
+}
+
+void bestFitDecreasing(vector<int>& blocks, vector<int>& answer, int& cnt, vector<int>& volumns) {
+    sort(blocks.begin(), blocks.end());
+    for(auto& block:blocks) {
+        bestFit(block, answer, cnt, volumns);
     }
 }
