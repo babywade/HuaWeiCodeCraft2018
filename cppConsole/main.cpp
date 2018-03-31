e7021d8b013e0a69f8c546ef0bf4437f2b19fde9
babywade <yixiaofei@stu.xjtu.edu.cn>
2018-03-31 15:59:01 +0800

    update

--- cppConsole/main.cpp
+++ cppConsole/main.cpp
@@ -3,11 +3,15 @@
 #include <vector>
 #include <algorithm>
 #include "hwpair.h"
+#include "Flavor.h"
+#include "TimeRecord.h"
+#include "DataRecord.h"
 
 using namespace std;
 //箱子体积
-#define VOLUMN_X 10
-#define VOLUMN_Y 10
+#define VOLUMN_X 56
+#define VOLUMN_Y 128
+#define RADIX 1024
 
 static hwpair VOLUMN(VOLUMN_X, VOLUMN_Y);
 
@@ -20,6 +24,97 @@ void firstFitDecreasing(vector<hwpair>& blocks, vector<int>& answer, int& cnt, v
 void bestFitDecreasing(vector<hwpair>& blocks, vector<int>& answer, int& cnt, vector<hwpair>& volumns);
 
 int main() {
+    //变量声明
+    int totalCPU, totalMemory, totalHardDiskStorage, totalFlavor;
+    Flavor flavor;
+    vector<Flavor> flavors;
+    string resourceName;
+    TimeRecord predictionBegin, predictionEnd;
+
+    //读入input_5flavors_cpu_7days.txt
+    ifstream fin("input_5flavors_cpu_7days.txt");
+    if (fin.fail()) {
+        cout << "failed to open input_5flavors_cpu_7days.txt" << endl;
+    }
+    fin >> totalCPU >> totalMemory >> totalHardDiskStorage >> totalFlavor;
+    for (int i = 0; i < totalFlavor; ++i) {
+        fin >> flavor;
+        flavors.push_back(flavor);
+    }
+    fin >> resourceName;
+    fin >> predictionBegin >> predictionEnd;
+    fin.close();
+
+//    测试读取
+//    cout << totalCPU << " " << totalMemory << " " << totalHardDiskStorage << endl
+//    << totalFlavor << endl;
+//    for (auto &flavor : flavors) {
+//        cout << flavor;
+//    }
+//    cout << resourceName << endl
+//    << predictionBegin << predictionEnd;
+
+//  预测
+    fin.clear();
+    fin.open("TrainData_2015.1.1_2015.2.19.txt");
+//    ifstream fpin("TrainData_2015.1.1_2015.2.19.txt");
+    DataRecord dataRecord;
+    int cntRecord = 0;
+    while(fin >> dataRecord) {
+        auto s = dataRecord.flavor.name.size();
+        string sub = dataRecord.flavor.name.substr(6, s - 1);
+//        cout << sub << " ";
+        int number = stoi(sub);
+//        cout << number << endl;
+        ++cntRecord;
+        switch(number) {
+        case 1:
+            ++flavors[0].cnt;
+            break;
+        case 2:
+            ++flavors[1].cnt;
+            break;
+        case 3:
+            ++flavors[2].cnt;
+            break;
+        case 4:
+            ++flavors[3].cnt;
+            break;
+        case 5:
+            ++flavors[4].cnt;
+            break;
+        default:
+            continue;
+        }
+    }
+    int a = flavors[0].cnt;
+    int b = flavors[1].cnt;
+    int c = flavors[2].cnt;
+    int d = flavors[3].cnt;
+    int e = flavors[4].cnt;
+
+    cout << "flavor1:" << a << endl
+    << "flavor2:" << b << endl
+    << "flavor3:" << c << endl
+    << "flavor4:" << d << endl
+    << "flavor5:" << e << endl
+    << "cntRecord:" << cntRecord << endl;
+
+    a = a * 7 / 50;
+    b = b * 7 / 50;
+    c = c * 7 / 50;
+    d = d * 7 / 50;
+    e = e * 7 / 50;
+    int sum = a + b + c + d + e;
+    cout << sum << endl;
+    cout << "flavor1 " << a << endl
+    << "flavor2 " << b << endl
+    << "flavor3 " << c << endl
+    << "flavor4 " << d << endl
+    << "flavor5 " << e << endl << endl;
+//  放置
+
+
     hwpair block;
     vector<int> answer;
     vector<hwpair> volumns;
